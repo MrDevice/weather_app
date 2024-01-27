ea308e6b1d07bdae2be2ee5fc007d7e56dee16c1
MrDevice <>
2024-01-27 17:08:02 +0100

    Minor changes to the functionallity

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const API_KEY = '273cf7b9eaf2ab7a2b55709dd22dce9f';
 
 let map_address = document.getElementById('city_address');
 let cityParagraph = document.getElementById('city');
+let cityTemp = document.getElementById('temp');
+let weather_type = document.getElementById('weather');
 
 let map_address_value;
 
 map_address.addEventListener('input', function(){
   map_address_value = map_address.value;
 
-const API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}';
 
 const GEO_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${map_address_value}&limit=5&appid=${API_KEY}`;
 
+    
 
 
 fetch(GEO_API_URL)
@@ -27,6 +29,22 @@ fetch(GEO_API_URL)
     console.log(data);
 
     cityParagraph.innerHTML = data[0].name;
+    const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${API_KEY}`;
+
+    fetch(API_URL)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+
+      })
+      .then(data => {
+        console.log(data);
+        cityTemp.innerHTML = Math.round(data.main.temp - 273).toFixed(2) + '°C';
+        weather_type.innerHTML = data.weather[0].main;
+        
+      })
 
   })
   .catch(error => {
@@ -34,3 +52,5 @@ fetch(GEO_API_URL)
   });
 
 });
+
+
